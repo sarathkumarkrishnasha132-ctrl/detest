7669f95fe28fd035c13e24048bbd15be14185471
Ksarath429@yahoo.com <Ksarath429@yahoo.com>
2026-06-08 02:20:17 +0000

    new

---
+++ models/example/staging/cleaning.sql
@@ -0,0 +1,14 @@
+select
+    upper(order_id) as order_id,
+    cast(order_date as date) as order_date,
+    trim(carrier) as carrier,
+    
+    case
+        when on_time = 1 then 'On Time'
+        else 'Late'
+    end as delivery_status,
+
+    freight_cost,
+    weight_kg
+
+from {{ source('RAW', 'datatable') }}
